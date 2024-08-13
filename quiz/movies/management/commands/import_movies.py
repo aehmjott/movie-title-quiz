ba83d9cd7d2b62b0724060823ae47fc503f47cbf
Matthias Johannsen <johannsen.matthias@web.de>
2024-08-14 00:07:38 +0200

    Remove script and json files

--- quiz/movies/management/commands/import_movies.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from movies.tasks import import_movies
-
-class Command(BaseCommand):
-    help = "Imports movie data from a json file"
-
-    def handle(self, *args, **options):
-        import_movies()
