426aa29b0b015dd899d3608fe5e2dbd1a1e53248
Matthias Johannsen <johannsen.matthias@web.de>
2024-08-15 00:19:56 +0200

    Add 'count' argument to import_wikidata_details command

--- quiz/movies/management/commands/import_wikidata_details.py
+++ quiz/movies/management/commands/import_wikidata_details.py
@@ -6,6 +6,9 @@ from movies.models import Movie
 class Command(BaseCommand):
     help = "Imports details from Wikidata for existing ``movies.Movie`` objects"
 
+    def add_arguments(self, parser):
+        parser.add_argument("count", type=int)
+
     def handle(self, *args, **options):
-        incomplete_movies = Movie.objects.filter(english_title="")[:10]
+        incomplete_movies = Movie.objects.filter(english_title="")[: options["count"]]
         WikidataAPI(incomplete_movies).run()
