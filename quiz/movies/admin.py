49d2ef34ea64840e23c2b6d8ead09c5720193945
Matthias Johannsen <johannsen.matthias@web.de>
2024-08-14 17:28:16 +0200

    Remove unused imports

--- quiz/movies/admin.py
+++ quiz/movies/admin.py
@@ -1,8 +1,5 @@
-from typing import Any
 from django.contrib import admin
 from django import forms
-from django.db.models.query import QuerySet
-from django.http import HttpRequest
 from .models import Person, Movie, AlternativeMovieTitle
 
 
